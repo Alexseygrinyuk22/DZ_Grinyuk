2bfed362adf0fea6e85f636ac4f42520ce4463db
Alexsey Grin <alexseygrinyuk@yandex.ru>
2024-08-23 23:24:56 +1100

    исправление кода

--- src/decorators.py
+++ src/decorators.py
@@ -2,13 +2,12 @@ from time import time
 from functools import wraps
 def log(filename):
     """Декоратор который логирует вызов функции и ее результат в файл или консоль"""
-
     def timer(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 time_1 = time()
-                result = func(*args, *kwargs)
+                result = (*args, *kwargs)
                 time_2 = time
                 if filename:
                     with open(filename, "a", encoding="utf-8") as file:
@@ -24,13 +23,10 @@ def log(filename):
                 raise
             return result
         return wrapper
-    return timer(func)
+    return timer('func')
 
 
 @log(filename="mylog.txt")
 def my_function(x, y):
     """Функция складывает два числа"""
     return x + y
-
-
-my_function(1, 2)
