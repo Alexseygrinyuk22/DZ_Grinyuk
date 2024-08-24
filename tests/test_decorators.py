52b594b24bf86aa7415a6a815be34f28668f471f
Alexsey Grin <alexseygrinyuk@yandex.ru>
2024-08-25 01:09:34 +1100

    исправление кода

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,8 +1,8 @@
 import pytest
-from src.decorators import log, my_function
+from src.decorators import log, func
 
 def test_log_exseption(capsys):
-        with open(my_function, 'r', encoding='utf-8') as file:
+        with open(func, 'r', encoding='utf-8') as file:
             logs = file.read()
         assert "my function error: unsupported operand type(s) for +: 'int' and 'str'. inputs: (1, '2'), {}" in logs
 
@@ -12,4 +12,4 @@ def test_log_exposition(capsys):
         return x+y
     func(1, "2")
     captured = capsys.readouterr()
-    assert "TypeError: unsupported operand type(s) for +: 'int' and 'str" in captured.out
+    assert "TypeError: unsupported operand type(s) for +: 'int' and 'str'" in captured.out
